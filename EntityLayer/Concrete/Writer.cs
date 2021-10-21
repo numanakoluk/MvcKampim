9f4d817ed8e3f109fce3c4c806fce6f671dbb647
numanakoluk <numanakoluk01@gmail.com>
2021-10-21 22:10:06 +0300

    Concrete class

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public int WriterName { get; set; }
+        public int WriterSurname { get; set; }
+        public int WriterImage { get; set; }
+        public int WriterMail { get; set; }
+        public int WriterPassword { get; set; }
+    }
+}
