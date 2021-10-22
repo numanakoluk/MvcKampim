e2af794deecaedec78242f8e69023ef57cc0f282
numanakoluk <numanakoluk01@gmail.com>
2021-10-22 19:14:31 +0300

    WriterController

---
+++ Mvc Kampim/Controllers/WritersController.cs
@@ -0,0 +1,20 @@
+﻿using BusinesssLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Kampim.Controllers
+{
+    public class WritersController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
