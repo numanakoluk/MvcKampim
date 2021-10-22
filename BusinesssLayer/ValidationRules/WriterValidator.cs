97c32cb631a0ee2648f2228873765aa7daeb7489
numanakoluk <numanakoluk01@gmail.com>
2021-10-22 21:55:06 +0300

    WriterTitle

--- BusinesssLayer/ValidationRules/WriterValidator.cs
+++ BusinesssLayer/ValidationRules/WriterValidator.cs
@@ -15,6 +15,7 @@ namespace BusinesssLayer.ValidationRules
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz");
             RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadını Boş Geçemezsiniz");
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda Kısmını Boş Geçemezsiniz");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan Kısmını Boş Geçemezsiniz");
             RuleFor(x => x.WriterName).MinimumLength(3).WithMessage("Lütfen En Az 2 Karekter Girişi Yapın");
             RuleFor(x => x.WriterName).MaximumLength(20).WithMessage("Lütfen En Fazla 20 Karekter Girişi Yapın");
         }
