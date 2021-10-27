fe0c1431e5baa697956275d0ad4f21d20e866bc5
numanakoluk <numanakoluk01@gmail.com>
2021-10-27 22:02:20 +0300

    ImageGalery

---
+++ BusinesssLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinesssLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinesssLayer.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDal _imageDal;
+
+        public ImageFileManager(IImageFileDal ımageDal)
+        {
+            _imageDal = ımageDal;
+        }
+
+        public List<ImageFile> GetList()
+        {
+           return _imageDal.List();
+        }
+    }
+}
